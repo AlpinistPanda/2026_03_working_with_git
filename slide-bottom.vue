ac86612c49694bb069776febb1a724875e410ddc
Ozgun Balaban <ozgunbalaban@yahoo.com>
2026-03-25 11:37:21 +0100

    feat: slide footer added

---
+++ slide-bottom.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer
+    class="bg-[#ff0000] absolute bottom-0 left-0 w-full flex items-center justify-between px-6 py-3 text-sm text-white z-20"
+  >
+    <!-- Left: logos -->
+    <div class="flex items-center gap-4">
+
+      <img
+        src="/img/logo.png"
+        alt="Coding Café"
+        class="h-8 object-contain"
+      />
+    </div>
+
+    <!-- Center: title -->
+    <div class="text-center font-medium tracking-wide">
+      BE Coding Café - 25 March 2026
+    </div>
+
+    <!-- Right: slide numbers -->
+    <div class="text-right tabular-nums">
+      {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+    </div>
+  </footer>
+</template>
